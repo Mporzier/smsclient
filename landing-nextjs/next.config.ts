bf9157e7bbfb8f766139e2a317104786d8e14b57
Mathias <mati2000@live.fr>
2026-02-20 20:46:40 +0100

    Fixed url

--- landing-nextjs/next.config.ts
+++ landing-nextjs/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export", // Tells Next.js to produce static files
-  basePath: "/smsclient", // ex: "" = racine, "/mon-site" = ...github.io/mon-site/
+  // Repo = smsclient → ...github.io/smsclient/
+  basePath: "/smsclient",
   images: {
     unoptimized: true,
   },
